d25ac98d81fd00704681f9a4af8a4c0536c45137
minkyungyeee <minkfree94@gmail.com>
2021-05-04 18:10:42 +0900

    create

---
+++ js/pofol.js
@@ -0,0 +1,32 @@
+;(function($){
+    var pofol = {
+        init:function(){
+            var that = this;
+                that.headerFn();
+                that.section1Fn();
+                that.section2Fn();
+                that.section3Fn();
+                that.section4Fn();
+                that.footerFn();
+        },
+        headerFn:function(){
+
+        },
+        section1Fn:function(){
+
+        },
+        section2Fn:function(){
+
+        },
+        section3Fn:function(){
+
+        },
+        section4Fn:function(){
+
+        },
+        footerFn:function(){
+
+        }
+    }
+    pofol.init();
+})(jQuery);
