5af6174ab2a761ee70ab446f1039cfe14cd9db49
minkyungyeee <minkfree94@gmail.com>
2021-05-21 00:33:56 +0900

    Update home

--- js/pofol.js
+++ js/pofol.js
@@ -48,17 +48,32 @@
             var $header = $('#header');
             var $nav = $('#nav');
             var $mainBtn = $('#header .main-btn');
+            var $mobileBtn = $('#header .mobile-btn');
 
-            $mainBtn.on({
-                mouseenter:function(){
-                    $header.addClass('over');
+            function pcEventFn(){
+                $nav.css({display:'inline-block'});
+
+                $mainBtn.on({
+                    mouseenter:function(){
+                        $header.addClass('over');
+                    }
+                });
+                $nav.on({
+                    mouseleave:function(){
+                        $header.removeClass('over');
+                    }
+                });
+            }
+
+            function mobileEventFn(){
+                
+            }
+            $mobileBtn.on({
+                click:function(){
+                    $header.toggleClass('over');
+                    $(this).toggleClass('addClick')
                 }
-            });
-            $nav.on({
-                mouseleave:function(){
-                    $header.removeClass('over');
-                }
-            });
+            })
         },
         section1Fn:function(){
             var win = $(window);
@@ -80,11 +95,6 @@
             var setId = null;
             var setId2 = null;
 
-            var touchStart = 0;
-            var touchEnd = 0;
-            var mouseDown = false;
-            var touchYstart = 0;
-            var touchYend = 0;
 
             function resizeFn(){
                 winW = $(window).innerWidth();
@@ -182,76 +192,20 @@
                 },1000);
             }
 
-            $slideView.on({
-                mousedown:function(e){
-                    mouseDown = 1;
-                    e.preventDefault();
-                    touchStart = e.pageX;
-                    touchYstart = e.pageY;
-                },
-                touchstart:function(e){
-                    mouseDown =1;
-                    e.preventDefault();
-                    touchStart = e.originalEvent.changedTouches[0].pageX;
-                    touchYstart = e.originalEvent.changedTouches[0].pageY;
-                },
-                mouseup:function(e){
-                    e.preventDefault();
-                    mouseDown = 0;
-                    touchEnd = e.pageX;
-                    touchYend = e.pageY;
-                    touchSwipeFn();
-
-                    if(touchYstart - touchYend < -50){
-                        $('html,body').stop().animate({scrollTop:0},1000);
-                    }
-                    if(touchYstart - touchYend > 50){
-                        $('html,body').stop().animate({scrollTop:$('#section2').offset().top},1000);
+            $slideView.swipe({
+                swipeLeft:function(){
+                    if(!$slideWrap.is(':animated')){
+                        pauseTimerFn()
+                        nextSlideCountFn();
                     }
                 },
-                touchend:function(e){
-                    e.preventDefault();
-                    mouseDown = 0;
-                    touchEnd = e.originalEvent.changedTouches[0].pageX;
-                    touchYend = e.originalEvent.changedTouches[0].pageY;
-                    console.log(touchYstart,touchYend)
-                    touchSwipeFn();
-                    //위에서 아래로 터치
-                    if(touchYstart - touchYend < -50){ 
-                        $('html,body').stop().animate({scrollTop:0},1000);
-                    }
-                    //아래에서 위로 터치
-                    if(touchYstart - touchYend > 50){ 
-                        $('html,body').stop().animate({scrollTop:$('#section2').offset().top},1000);
-                    }
-                },
-                mouseleave:function(e){
-                    e.preventDefault();
-                    if(mouseDown == 1){
-                        mouseDown = 0;
-                        touchEnd = e.pageX;
-                        touchSwipeFn();
+                swipeRight:function(){
+                    if(!$slideWrap.is(':animated')){
+                        pauseTimerFn()
+                        prevSlideCountFn();
                     }
                 }
             });
-
-            function touchSwipeFn(){
-                //console.log('touchStart',touchStart);
-                //console.log('touchEnd',touchEnd);
-
-                if(touchStart-touchEnd>0){
-                        pauseTimerFn();
-                        if(!$slide.is(':animated')){
-                            nextSlideCountFn();
-                        }
-                }
-                if(touchStart-touchEnd<0){
-                        pauseTimerFn();
-                        if(!$slide.is(':animated')){
-                            prevSlideCountFn();
-                        }
-                }
-            }
         },
         section1NoticeFn:function(){
             //notice-rolling
