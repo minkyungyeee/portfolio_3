4d747aed7a26c9cbb4b221c2e76d016d5bebc024
minkyungyeee <minkfree94@gmail.com>
2021-05-24 00:42:37 +0900

    Update

--- js/pofol.js
+++ js/pofol.js
@@ -45,16 +45,25 @@
             });
         },
         headerFn:function(){
+            var $html = $('html');
             var $header = $('#header');
             var $nav = $('#nav');
             var $sub = $('#nav .sub');
             var $mainBtn = $('#header .main-btn');
             var $mobileBtn = $('#header .mobile-btn');
+            var $mClose = $('#nav .m-close')
             var $subShow = $('#wrap .sub-show');
             var pc = 0;
             var mo = 0;
 
             function pcEventFn(){
+                //moblie상태에서 pc로 갓을때 다 초기화
+                $header.removeClass('over');
+                $html.removeClass('addSub');
+                $subShow.removeClass('addSubActive'); //검은 배경추가
+                $mobileBtn.removeClass('addClick');
+                $mClose.removeClass('addDepth')
+
                 $nav.css({display:'inline-block'});
 
                 $mainBtn.on({
@@ -72,7 +81,7 @@
             }
 
             function mobileEventFn(){
-                $sub.stop().hide();
+                //$sub.stop().hide(); 이거있으면 리사이즈될때마다 서브가 사라져버려서 없앰
 
                 $mainBtn.off('mouseenter');
                 $nav.off('mouseleave');
@@ -96,31 +105,30 @@
                 setTimeout(pcMoEventFn,100);
             });
 
+            $mobileBtn.on({
+                click:function(){
+                    $header.toggleClass('over');
+                    $html.toggleClass('addSub');
+                    $subShow.toggleClass('addSubActive'); //검은 배경추가
+                    $(this).toggleClass('addClick');
+                }
+            });
+
             $mainBtn.each(function(idx){
                 $(this).on({
                     click:function(e){
                         e.preventDefault();
                         if(mo==1){
                             $sub.stop().slideUp(300);
-                            
-    
-                            if(!$mainBtn.eq(idx).children().hasClass('addDepth')){
+                            if(!$mainBtn.eq(idx).children().hasClass('addDepth')){ //다른거 누르면 무조건 초기화
                                 $mainBtn.children().removeClass('addDepth')
                             }
-                            $(this).next().stop().slideToggle(300);
-                            
-                            $(this).children().toggleClass('addDepth');
+                            $(this).next().stop().slideToggle(300); //서브메뉴
+                           
+                            $(this).children().toggleClass('addDepth'); //서브메뉴 +기호 모양변경
                         }
                     }
                 })
-            });
-
-            $mobileBtn.on({
-                click:function(){
-                    $header.toggleClass('over');
-                    $subShow.toggleClass('addSubActive');
-                    $(this).toggleClass('addClick');
-                }
             });
         },
         section1Fn:function(){
@@ -342,23 +350,51 @@
             var $prevBtn = $('#section2 .prev-btn');
             var $nextBtn = $('#section2 .next-btn');
             var cnt = 0;
+            var i = 0; //화면크기에따른 슬라이드 횟수제한 변수
 
             function resizeFn(){
                 winW = $(window).innerWidth();
                 winH = $(window).innerHeight();
+                slideW = $('#section2 .sales-slide').innerWidth();
 
                 $sec2.css({width:winW,height:winH});
+                if(winW > 1800){ //4개씩 한 화면
+                    i = 3;
+                }
+                else if(winW > 1020){ //3개씩 한 화면
+                    i = 4;
+                }
+                else if(winW > 770){ //2개씩 한 화면
+                    i = 5;
+                }
+                else {  //1개씩 한 화면
+                    i = 6;
+                }
             }
 
             setTimeout(resizeFn,100);
 
             win.resize(function(){
                 setTimeout(resizeFn,100);
+                //setTimeout(mainSlideFn,100); //리사이즈될때마다 넓이값 맞게 움직임
+                $slideUl.stop().animate({left:-slideW * cnt},0)
             });
 
-/*             function mainSlideFn(){
+            function mainSlideFn(){
                 slideW = $('#section2 .sales-slide').innerWidth();
-                $slideUl.css({transform:'translate3d(' + -33.3333+'%'*cnt + ',0,0)'})
+                $slideUl.stop().animate({left:-slideW * cnt})
+                console.log(i)
+                if(cnt==0){
+                    $prevBtn.css({opacity:0.5}).off('click');
+                }
+                if(cnt>0 && cnt<i){
+                    $prevBtn.css({opacity:1}).on('click')
+                    $nextBtn.css({opacity:1}).on('click')
+                }
+                if(cnt==i){
+                    $nextBtn.css({opacity:.5}).off('click');
+                    $prevBtn.css({opacity:1}).on('click')
+                }
             }
 
             function nextCountFn(){
@@ -374,6 +410,7 @@
             $prevBtn.on({
                 click:function(){
                     if(!$slideUl.is(':animated')){
+                        //chk --;
                         prevCountFn();
                     }
                 }
@@ -382,10 +419,11 @@
             $nextBtn.on({
                 click:function(){
                     if(!$slideUl.is(':animated')){
+                        //chk ++;
                         nextCountFn();
                     }
                 }
-            }); */
+            });
 
             $salesSlide.each(function(idx){
                 $(this).on({
